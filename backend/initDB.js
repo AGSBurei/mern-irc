d55ef31a191e158dcd738e416368f0eb76e6a47d
eto <alainbay13@gmail.com>
2021-01-05 14:27:52 +0100

    create  file

---
+++ backend/initDB.js
@@ -0,0 +1,31 @@
+const { connect, connection } = require('mongoose');
+const { config } = require('dotenv'); 
+
+module.exports = () => {
+    config();
+    const uri = process.env.DB_URI;
+
+    connect(uri, {
+        dbName: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASS,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    }).then(() => {
+        console.log('Connection estabislished with MongoDB');
+    }).catch(error => console.error(error.message));
+
+    connection.on('connected', () => {
+        console.log('Mongoose connected to DB Cluster');
+    })
+
+    connection.on('error', (error) => {
+        console.error(error.message);
+    })
+
+    connection.on('disconnected', () => {
+        console.log('Mongoose Disconnected');
+    })
+}
