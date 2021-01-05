faa4180479f4317654c45518ca992f7f1bad024c
eto <alainbay13@gmail.com>
2021-01-05 15:26:46 +0100

    change initDB.js

--- backend/initDB.js
+++ backend/initDB.js
@@ -1,19 +1,21 @@
-const { connect, connection } = require('mongoose');
-const { config } = require('dotenv'); 
+const mongoose = require('mongoose');
 
 module.exports = () => {
-    config();
     const uri = process.env.DB_URI;
 
-    connect(uri, {
-        dbName: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        pass: process.env.DB_PASS,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    }).then(() => {
-        console.log('Connection estabislished with MongoDB');
-    }).catch(error => console.error(error.message));
+    mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+    const connection = mongoose.connection;
+
+    connection.once('open', () => {
+        console.log("MongoDB : database connection");
+    })
+
+    connection.on('error', (error) => {
+        console.error(error.message);
+    })
+
+    connection.on('disconnected', () => {
+        console.log("MongoDB : database disconnected");
+    })
 }
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.SERVER_PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -10,5 +12,6 @@ app.use(express.json());
 require('./initDB')();
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+    console.log('Server : Start')
+    console.log(`Port: ${port}`);
 });
