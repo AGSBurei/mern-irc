bfe9be209daf6e90c5ac25c6f6ce03462d0726eb
eto <alainbay13@gmail.com>
2021-02-02 15:59:24 +0100

    add log in and sign up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Chat from "./pages/Chat/Chat.js";
 import login from "./pages/login/login.js";
-import signup from "./pages/signup/signup.js";
 import * as React from "react";
 
 export default () => {
@@ -10,7 +9,6 @@ export default () => {
       <Switch>
         <Route path="/chat" component={Chat} />
         <Route path="/login" component={login} />
-        <Route path="/signup" component={signup} />
       </Switch>
     </BrowserRouter>
   );
--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -1,37 +1,64 @@
 import * as React from "react";
+import './login.css';
 
-const axios = require('axios');
+const login = () => {
+    return (
+        <div class="container">
+            <div class="main">
+                <div class="input">
+                    <div class="title">
+                        <img src="./anti-rain.png" alt="anti-rain"></img>
+                        <label>ANTI RAIN</label>
+                    </div>
 
-const loginTo = () => { 
-    axios.get('http://localhost:5000/user', {
-        params: {
-            mail:"a",
-            password:"a",
-        }
-    }).then((response) => {
-        console.log(response);
-    }).catch((error) => {
-        console.log(error);
-    }).then(function () {
-        // always executed
-    });
-}
+                    <div id="login">
+                        <div class="email">
+                            <label for="email">E-Mail</label>
+                            <input type="email" name="email" autoComplete="off"></input>
+                        </div>
 
-const login = () => {   
-    return(
-        <div>
-            <div>
-                <label>email :</label>
-                <input type="text"></input>
-            </div>
+                        <div class="password">
+                            <label for="password" >Passord</label>
+                            <input type="password" name="password" autoComplete="off"></input>
+                        </div>
 
-            <div>
-                <label>password :</label>
-                <input type="text"></input>
-            </div>
+                        <div class="button">
+                            <button>Log in</button>
+                            <label> Want to <button class="show">register</button> ?</label>
+                        </div>
+                    </div>
 
-            <div>
-                <button onClick={loginTo()}>login</button>
+                    <div id="signup">
+                        <div class="name">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" autoComplete="off"></input>
+                        </div>
+
+                        <div class="email">
+                            <label for="email">E-Mail</label>
+                            <input type="email" name="email" autoComplete="off"></input>
+                        </div>
+
+                        <div class="password">
+                            <label for="password">Passord</label>
+                            <input type="password" name="password" autoComplete="off"></input>
+                        </div>
+
+                        <div class="password">
+                            <label for="password-confirm">Confirm passord</label>
+                            <input type="password" name="password-confirm" autoComplete="off"></input>
+                        </div>
+
+                        <div class="button">
+                            <button>Register</button>
+                            <label> Want to <button  class="show">log in</button> ?</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="image">
+                    <img src="./a10.png" alt="image"></img>
+                </div>
             </div>
         </div>
     )
